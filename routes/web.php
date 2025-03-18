308c143ee86985084dd3012231a1077012d3b029
MOHAMED AYOUB TAJMOUT <m.tajmout@eca-assurances.com>
2025-03-18 12:05:35 +0000

    test email

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test-email', function () {
+    try {
+        Mail::raw('This is a test email from Laravel app.', function ($message) {
+            $message->to('mohamed.tajmout@gmail.com')
+                    ->from(config('mail.from.address'), config('mail.from.name'))
+                    ->subject('Test Email from App');
+        });
+
+        return 'Email sent successfully!';
+    } catch (\Exception $e) {
+        return 'Error: ' . $e->getMessage();
+    }
 });
